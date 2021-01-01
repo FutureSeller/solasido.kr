50be61ab6ce51d0aa6faa0509b8ae95072b3dd77
FutureSeller <f.s3ll3r@gmail.com>
2021-01-01 22:32:10 +0900

    FP-5 Add font-families

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,8 @@ import SEO from '../components/SEO'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1 style={{ fontFamily: 'lovelo' }}>Hi people</h1>
+    <p style={{ fontFamily: 'Source Sans Pro'}}>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }} />
     <Link to="/404/">Go to page 2</Link> <br />
