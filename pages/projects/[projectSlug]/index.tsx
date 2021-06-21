2781155256c5c13025d4027221b28991c3406db3
FutureSeller <f.s3ll3r@gmail.com>
2021-06-21 19:16:23 +0900

    issue-2 Add Meta component

--- pages/projects/[projectSlug]/index.tsx
+++ pages/projects/[projectSlug]/index.tsx
@@ -1,3 +1,8 @@
+import fs from 'fs'
+import path from 'path'
+import Image from 'next/image'
+
+import Meta from '../../../components/common/Meta'
 import Container from '../../../components/common/Container'
 
 import projectsJson from '../../../public/data/projects.json'
@@ -9,17 +14,53 @@ interface Props {
   project: Project
   prevProjectSlug: string | null
   nextProjectSlug: string | null
+  images: string[]
 }
 
-export default function ProjectPage({ project, prevProjectSlug, nextProjectSlug }: Props) {
+export default function ProjectPage({ project, prevProjectSlug, nextProjectSlug, images }: Props) {
   return (
-    <Container>
-      <pre>
-        {JSON.stringify(project, null, 2)}
-      </pre>
-      {`prev : ${prevProjectSlug}`}
-      {`next : ${nextProjectSlug}`}
-    </Container>
+    <>
+      <Meta title={project.title} description={project.summary.join(',')} />
+      <Container>
+        <button>All Projects</button>
+        <h1>{project.title}</h1>
+        <ul>
+          {project.category.map(cat => (<li>{cat}</li>))}
+        </ul>
+        <div>
+          {project.summary.map(sum => (<div>{sum}</div>))}
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <div style={{ width: '45%' }}>
+            <ul>
+              <li>{project.client}</li>
+              <li>{project.team}</li>
+              <li>{project.startDate} ~ {project.endDate}</li>
+              <li>{project.role.join(', ')}</li>
+            </ul>
+            {project.description.map(desc => (<p>{desc}</p>))}
+          </div>
+          <div style={{ width: '45%' }}>
+            <ul>
+              {images.map(filename => (
+                <li style={{ width: 530, height: 353, position: 'relative' }}>
+                  <Image
+                    src={`/images/${project.slug}/${filename}`}
+                    alt="close-menu"
+                    layout="fill"
+                  />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <footer>
+          {prevProjectSlug && '<-'}
+          {project.title}
+          {nextProjectSlug && '->'}
+        </footer>
+      </Container>
+    </>
   )
 }
 
@@ -43,6 +84,8 @@ export const getStaticProps: GetStaticProps = async (context: GetStaticPropsCont
     }
   }
 
+  const images = fs.readdirSync(path.join(process.cwd(), `/public/images/${context.params?.projectSlug}/`))
+    .filter(filename => !filename.includes('thumbnail'))
   const prevProjectSlug = projectIndex === 0 ? null : projects[projectIndex - 1].slug
   const nextProjectSlug = projectIndex === projects.length - 1 ? null : projects[projectIndex + 1].slug
 
@@ -51,6 +94,7 @@ export const getStaticProps: GetStaticProps = async (context: GetStaticPropsCont
       project: projects[projectIndex],
       prevProjectSlug,
       nextProjectSlug,
+      images,
     }
   }
 }
