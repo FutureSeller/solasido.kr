d1dbd2151158f63472c9f8cb44489df637170199
FutureSeller <f.s3ll3r@gmail.com>
2021-07-02 01:41:55 +0900

    issue-24 Add hover and active effect

--- components/project/WorkItem.tsx
+++ components/project/WorkItem.tsx
@@ -1,4 +1,4 @@
-import { css } from '@emotion/react'
+import { css, keyframes } from '@emotion/react'
 import styled from '@emotion/styled'
 import Link from 'next/link'
 
@@ -7,6 +7,8 @@ import AspectRatioImage from '../common/AspectRatioImage'
 import { responsive } from '../../styles/responsive'
 import { notoSansKR } from '../../styles/typography'
 import { Thumbnail } from '../../types/model'
+import { colors } from '../../styles/colors'
+import { zIndex } from '../../styles/zIndex'
 
 interface Props {
   item: Thumbnail
@@ -18,11 +20,13 @@ export default function WorkItem({ item, isLargeImage = false, isSublistItem = f
   return (
     <Link href={`/projects/${item.slug}`} passHref>
       <Container isSublistItem={isSublistItem}>
-        <AspectRatioImage
-          src={item.src}
-          alt={item.alt}
-          isLargeImage={isLargeImage}
-        />
+        <StrikeThrough style={{ width: '100%', height: '100%', position: 'relative' }}>
+          <AspectRatioImage
+            src={item.src}
+            alt={item.alt}
+            isLargeImage={isLargeImage}
+          />
+        </StrikeThrough>
         <ContentFont>
           <Description>{item.description}</Description>
         </ContentFont>
@@ -32,11 +36,13 @@ export default function WorkItem({ item, isLargeImage = false, isSublistItem = f
 }
 
 const Container = styled.article<{ isSublistItem: boolean }>`
+  position: relative;
   width: calc(50% - 8px);
 
-  &:hover {
+  &:hover, &:active {
     text-decoration: underline;
     text-decoration-thickness: 2px;
+    cursor: pointer;
   }
 
   ${responsive.mdLte} {
@@ -66,4 +72,35 @@ const ContentFont = styled.div`
 
 const Description = styled.p`
   padding-top: 12px;
+`
+
+// TODO: animation component로 이사가도 될 듯합니다.
+const mimicStrikeThrough = keyframes`
+  from {
+    width: 0;
+  }
+  to {
+    width: 100%;
+  }
+`
+
+const StrikeThrough = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+
+  &:hover, &:active {
+    &::before {
+      position: absolute;
+      left: 0;
+      top: 50%;
+      content: '';
+      transform: translateY(-50%);
+      
+      border-bottom: 15px solid ${colors.red};
+      animation: ${mimicStrikeThrough} 0.5s forwards;
+      animation-delay: 0.5s;
+      z-index: ${zIndex.content};
+    }
+  }
 `
