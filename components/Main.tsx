7c24a1a9ae817ac94372e0057575fdfda6e4c401
FutureSeller <f.s3ll3r@gmail.com>
2021-08-03 00:09:44 +0900

    Add Typing Animation

--- components/Main.tsx
+++ components/Main.tsx
@@ -2,43 +2,47 @@ import { keyframes } from '@emotion/react'
 import styled from '@emotion/styled'
 
 import Container from './common/Container'
-import Heading from './common/Heading'
-import Content from './common/Content'
 import NeueDisplay from './typography/NeueDisplay'
-import NeueBold from './typography/NeueBold'
-import Blink from './animation/Blink'
+import Typing from './animation/Typing'
 
 import { responsive } from '../styles/responsive'
 import { colors } from '../styles/colors'
 
 export default function Main() {
   return (
-    <Container>
+    <MainContainer>
       <HidedTitle>Solasido&apos;s Portfolio</HidedTitle>
-      <Heading isMainSection>
-        <NeueDisplay>
-          <Blink duration={1.8} delay={0.5}>
-            <div>Better Design,</div>
-            <div>Better Life.</div>
-          </Blink>
-          <Blink duration={1.8} delay={1}>
-            <StrikeThrough>Portfolio</StrikeThrough>
-          </Blink>
-        </NeueDisplay>
-      </Heading>
       <Content>
-        <NeueBold>
-          <Blink duration={0.5} delay={1.5}>
-            <SubjectWrapper>
-              <Subject>BRANDING</Subject>
-              <Subject>IDENTITY DESIGN</Subject>
-            </SubjectWrapper>
-          </Blink>
-        </NeueBold>
+        <NeueDisplay>
+          <Typing duration={1.2}>Better Design,</Typing>
+          <Typing duration={1.2} delay={3}>Better Life.</Typing>
+          <StrikeThrough />
+        </NeueDisplay>
       </Content>
-    </Container>
+    </MainContainer>
   )
 }
+
+const MainContainer = styled(Container)`
+position: relative;
+  height: 100vh;
+  background-color: ${colors.black};
+  color: ${colors.white};
+`
+
+const HidedTitle = styled.h1`
+  position: absolute;
+  overflow: hidden;
+  clip: rect(1px, 1px, 1px, 1px);
+  white-space: nowrap;
+`
+
+
+const Content = styled.div`
+  position: absolute;
+  display: inline-block;
+  top: 50%;
+`
 
 const mimicStrikeThrough = keyframes`
   from {
@@ -59,7 +63,7 @@ const StrikeThrough = styled.div`
     content: '';
     border-bottom: 25px solid ${colors.red};
     animation: ${mimicStrikeThrough} 1s forwards;
-    animation-delay: 1.5s;
+    animation-delay: 4.5s;
 
     ${responsive.lgLte} {
       border-bottom: 22px solid ${colors.red};
@@ -77,39 +81,4 @@ const StrikeThrough = styled.div`
       border-bottom: 7px solid ${colors.red};
     }
   }
-`
-
-const SubjectWrapper = styled.div`
-  display: inline-block;
-  padding-top: 240px;
-
-  ${responsive.mdLte} {
-    padding-top: 244px;
-  }
-
-  ${responsive.smLte} {
-    padding-top: 194px;
-  }
-`
-
-const Subject = styled.div`
-  border-bottom: 4px solid ${colors.black};
-  padding-top: 20px;
-
-  ${responsive.mdLte} {
-    border-bottom: 2.5px solid ${colors.black};
-    padding-top: 12.5px;
-  }
-
-  ${responsive.smLte} {
-    border-bottom: 2px solid ${colors.black};
-    padding-top: 10px;
-  }
-`
-
-const HidedTitle = styled.h1`
-  position: absolute;
-  overflow: hidden;
-  clip: rect(1px, 1px, 1px, 1px);
-  white-space: nowrap;
 `
---
+++ components/animation/Typing.tsx
@@ -0,0 +1,57 @@
+import { useRef, useEffect } from 'react'
+import styled from '@emotion/styled'
+import { keyframes } from '@emotion/react'
+
+interface Props {
+  duration: number
+  delay?: number
+  children: React.ReactNode
+}
+
+export default function Typing({ duration, delay, children }: Props) {
+  const divRef = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    if (!divRef.current) {
+      return
+    }
+
+    const typingDiv = divRef.current
+
+    const handleAnimationEnd = () => {
+      const style = typingDiv.style
+      if (!style) {
+        return
+      }
+      style.whiteSpace = 'normal'
+    }
+
+    typingDiv.addEventListener('animationend', handleAnimationEnd)
+
+    return () => {
+      typingDiv.removeEventListener('animationend', handleAnimationEnd)
+    }
+  }, [])
+
+  return (
+    <TypingWrapper duration={duration} delay={delay} ref={divRef}>
+      {children}
+    </TypingWrapper>
+  )
+}
+
+const mimicType = keyframes`
+  from { 
+    width: 0;
+  }
+  to { 
+    width: 100%;
+  }
+`
+
+const TypingWrapper = styled.div<{ duration: number, delay?: number }>`
+  overflow: hidden;
+  white-space: nowrap;
+  animation: ${mimicType} ${({ duration }) => `${duration}s`} steps(40) 1s 1 normal both;
+  animation-delay: ${({ delay }) => delay ? `${delay}s` : null};
+`
