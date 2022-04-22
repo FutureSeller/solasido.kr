fabfbc284caf1e003f0ecaf1c28d59dec1cda1fc
Jihoon Kim <f.s3ll3r@gmail.com>
2022-04-23 00:08:17 +0900

    feat: renewal about page (#66)
    * feat: wIP
    
    * feat: renewal about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,18 +1,16 @@
 import styled from '@emotion/styled'
-import { Box as ChakraBox, Flex } from '@chakra-ui/react'
+import { Box, Flex } from '@chakra-ui/react'
 
 import DeviceContextProvider from '../contexts/DeviceProvider'
 
 import Meta from '../components/Meta'
-import NavBar from '../components/NavBar'
 import ContentBox from '../components/ContentBox'
-import Footer from '../components/Footer'
-import PageTitle from '../components/PageTitle'
+import Marquee from '../components/Marquee'
+import NavBar from '../components/home/NavBar'
+import Footer from '../components/home/Footer'
 
-import Description from '../components/about/Description'
 import Experience from '../components/about/Experience'
 
-import { responsive } from '../styles/responsive'
 import { initializeApollo } from '../apollo/client'
 import {
   AboutPage_AwardsDocument,
@@ -27,57 +25,124 @@ import type {
 
 export default function AboutPage() {
   return (
-    <ChakraBox as="main" height="100%">
+    <>
       <Meta title="About | SOLASIDO" description="Better Design, Better Life." />
-      <NavBar color="white" backgroundColor="black" alwaysVisible />
-      <ContentBox as="section" backgroundColor="black" color="white">
-        <PageTitle>About</PageTitle>
-        <Stack>
-          <Box>HANSOL CHUNG</Box>
-          <Box>Brand + Visual Designer</Box>
-        </Stack>
-        <Description />
-      </ContentBox>
-      <DeviceContextProvider>
-        <Experience />
-      </DeviceContextProvider>
-      <Footer color="white" backgroundColor="black" />
-    </ChakraBox>
+      <Box as="main" height="100%" backgroundColor="black" minWidth="320px">
+        <NavBar />
+        <StyledContentBox as="section" backgroundColor="black" color="white">
+          <Stack>
+            <StyledNameBox>HANSOL CHUNG</StyledNameBox>
+            <StyledNameBox>Brand + Visual Designer</StyledNameBox>
+          </Stack>
+          <StyledDescription>
+            {`디자인이란 사람들에게 이야기하고자 하는 바를 시각적으로 어떻게 전달하는 것이\n효율적인가를 따지는 실속 문학이라고 합니다.\n브랜드의 가치를 시각화하고 브랜드와 소비자, 온라인과 오프라인을 연결하여\n가치있는 실속 문학을 하고자합니다.`}
+          </StyledDescription>
+        </StyledContentBox>
+        <StyledMarqueeBox>
+          <Marquee speed={400}>
+            <StyledMotionDiv>BRAND DESIGNER + VISUAL DESIGNER +&nbsp;</StyledMotionDiv>
+          </Marquee>
+          <Divider />
+          <Marquee speed={300} direction="right">
+            <StyledMotionDiv>SEOUL BASED DESIGNER&nbsp;</StyledMotionDiv>
+          </Marquee>
+          <Divider />
+          <Marquee speed={400}>
+            <StyledMotionDiv>FOCUS ON - BASIC - CLEARNESS - IDENTITY - &nbsp;</StyledMotionDiv>
+          </Marquee>
+        </StyledMarqueeBox>
+        <DeviceContextProvider>
+          <Experience />
+        </DeviceContextProvider>
+        <Footer color="white" backgroundColor="black" />
+      </Box>
+    </>
   )
 }
 
-const Stack = styled(Flex)`
-  margin-bottom: 6vw;
+const StyledMotionDiv = styled(Box)`
+  display: inline-block;
+  font-family: Gordita Bold;
+  font-size: 70px;
+  background: #000;
+  color: #000;
+  white-space: nowrap;
+  text-shadow: -1px -1px 0 #fff, 1px -1px 0 #fff, -1px 1px 0 #fff, 1px 1px 0 #fff;
+  margin: 20px 0;
 
-  ${responsive.mdLte} {
-    flex-direction: column;
-    margin-bottom: 60px;
+  @media (min-width: 768px) {
+    font-size: 100px;
   }
 
-  & > :not(style) ~ :not(style) {
-    margin-inline-start: 100px;
+  @media (min-width: 1920px) {
+    font-size: 120px;
+  }
+`
 
-    ${responsive.mdLte} {
-      margin: 0;
-      margin-top: 16px;
+const StyledContentBox = styled(ContentBox)`
+  padding-top: 76px;
+
+  @media (min-width: 768px) {
+    padding-top: 90px;
+  }
+
+  @media (min-width: 1920px) {
+    padding-top: 128px;
+  }
+`
+
+const Stack = styled(Flex)`
+  margin-bottom: 60px;
+  flex-direction: column;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
+`
+
+const StyledNameBox = styled.div`
+  font-weight: 700;
+  font-size: 20px;
+
+  @media (min-width: 768px) {
+    font-size: 26px;
+
+    &:first-of-type {
+      margin-right: 40px;
+    }
+  }
+
+  @media (min-width: 1920px) {
+    font-size: 40px;
+
+    &:first-of-type {
+      margin-right: 50px;
     }
   }
 `
 
-const Box = styled.div`
-  font-weight: 700;
-  font-size: 2vw;
+const Divider = styled.hr`
+  color: #fff;
+  height: 2px;
+`
 
-  ${responsive.lgLte} {
-    font-size: 3vw;
+const StyledDescription = styled(Box)`
+  font-size: 22px;
+  line-height: 34px;
+  white-space: pre-wrap;
+  margin-bottom: 20px;
+`
+
+const StyledMarqueeBox = styled(Box)`
+  background: #000;
+  padding-bottom: 64px;
+
+  @media (min-width: 768px) {
+    padding-bottom: 120px;
   }
 
-  ${responsive.mdLte} {
-    font-size: 3.5vw;
-  }
-
-  ${responsive.smLte} {
-    font-size: 5.5vw;
+  @media (min-width: 1920px) {
+    padding-bottom: 0;
   }
 `
 
