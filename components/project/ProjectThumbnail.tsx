3c4cce4ba8c26a1be0e023dea7c92fe1c793e2bd
Jihoon Kim <f.s3ll3r@gmail.com>
2022-04-30 23:24:31 +0900

    refactor: change title, summary style (#72)
    * refactor: change title, summary style
    
    * refactor: add spacing to thumbbnail

--- components/project/ProjectThumbnail.tsx
+++ components/project/ProjectThumbnail.tsx
@@ -3,7 +3,6 @@ import { Box, Heading, Text, AspectRatio } from '@chakra-ui/react'
 import Link from 'next/link'
 import Image from 'next/image'
 
-import { responsive } from '../../styles/responsive'
 import projectsData from '../../public/data/projects.json'
 
 type ArrayElement<A> = A extends readonly (infer T)[] ? T : never
@@ -19,7 +18,7 @@ export default function ProjectThumbnail({ figure }: Props) {
   return (
     <Link href={`/project/${slug}`} key={title} passHref prefetch={false}>
       <a>
-        <AspectRatio ratio={isPortrait ? 888 / 1108 : 888 / 592} marginBottom="16px">
+        <StyledAspectRatio ratio={isPortrait ? 888 / 1108 : 888 / 592}>
           <HoverImage
             src={thumbnail}
             layout="fill"
@@ -28,7 +27,7 @@ export default function ProjectThumbnail({ figure }: Props) {
             placeholder="blur"
             blurDataURL={placeholder}
           />
-        </AspectRatio>
+        </StyledAspectRatio>
         <StyledBox>
           <Heading as="h2">
             <Title>{title}</Title>
@@ -40,32 +39,50 @@ export default function ProjectThumbnail({ figure }: Props) {
   )
 }
 
+const StyledAspectRatio = styled(AspectRatio)`
+  margin-bottom: 12px;
+
+  @media (min-width: 768px) {
+    margin-bottom: 16px;
+  }
+`
+
 const Title = styled(Text)`
-  margin-bottom: 10px;
+  margin-bottom: 4px;
   font-family: Gordita Regular;
   font-weight: 400;
-  font-size: 16px;
+  font-size: 10px;
+
+  @media (min-width: 768px) {
+    margin-bottom: 6px;
+    font-size: 14px;
+  }
+
+  @media (min-width: 1920px) {
+    margin-bottom: 10px;
+    font-size: 16px;
+  }
 `
 
 const Summary = styled(Text)`
   margin: 0;
   font-family: Gordita Bold;
-  font-size: 24px;
+  font-size: 18px;
 
   @media (min-width: 768px) {
     font-size: 26px;
   }
 
   @media (min-width: 1920px) {
-    font-size: 50px;
+    font-size: 40px;
   }
 `
 
 const StyledBox = styled(Box)`
-  margin-bottom: 50px;
+  margin-bottom: 30px;
 
-  ${responsive.mdLte} {
-    margin-bottom: 30px;
+  @media (min-width: 768px) {
+    margin-bottom: 50px;
   }
 `
 
