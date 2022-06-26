4a337cabd6338f34dc5c3c0ebf1dd9027cc67cbc
FutureSeller <f.s3ll3r@gmail.com>
2022-06-27 02:50:18 +0900

    feat: add meta to wechelin

--- pages/project/wechelin.tsx
+++ pages/project/wechelin.tsx
@@ -7,6 +7,7 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 import IcRightArrow from '../../public/assets/right-arrow.svg'
 
+import Meta from '../../components/Meta'
 import Footer from '../../components/pageLayout/Footer'
 
 import ProjectIntroSection from '../../components/[project]/wechelin/ProjectIntroSection'
@@ -99,136 +100,139 @@ export default function WechelinPage({ locale }: Props) {
   const { t } = useTranslation(['common', 'wechelin'])
 
   return (
-    <Box as="main" position="relative" width="100%" height="100%" minHeight="100vh">
-      <ProjectIntroSection
-        title={t('wechelin:title')}
-        fixedTitleColor="white"
-        stickyTitleColor="black"
-        coverImage={
-          <Image
-            src="/projects/wechelin/main.jpg"
-            alt={t('wechelin:title')}
-            layout="fill"
-            objectFit="cover"
-            objectPosition="left"
-            placeholder="blur"
-            blurDataURL={GENERATED_PLACE_HOLDER}
+    <>
+      <Meta title={t('wechelin:title')} description="Our Michelin Guide" imageUrl="/projects/wechelin/main.jpg" />
+      <Box as="main" position="relative" width="100%" height="100%" minHeight="100vh">
+        <ProjectIntroSection
+          title={t('wechelin:title')}
+          fixedTitleColor="white"
+          stickyTitleColor="black"
+          coverImage={
+            <Image
+              src="/projects/wechelin/main.jpg"
+              alt={t('wechelin:title')}
+              layout="fill"
+              objectFit="cover"
+              objectPosition="left"
+              placeholder="blur"
+              blurDataURL={GENERATED_PLACE_HOLDER}
+            />
+          }
+        />
+        <NewExplainSection
+          backgroundColor="white"
+          color="black"
+          title={t('wechelin:title')}
+          descriptions={t('wechelin:summary.descriptions', { returnObjects: true })}
+          scopes={['Brand Design', 'Design Guidelines', 'UXUI', 'Mobile App']}
+          metaItems={
+            <>
+              <Box>
+                <Heading as="h3">
+                  <CategoryTitle>{t('wechelin:summary.work.type')}</CategoryTitle>
+                </Heading>
+                {t('wechelin:summary.work.value')}
+              </Box>
+              <Box>
+                <Heading as="h3">
+                  <CategoryTitle>PERIOD</CategoryTitle>
+                </Heading>
+                {t('wechelin:summary.period')}
+              </Box>
+            </>
+          }
+        />
+        <AspectRatio ratio={768 / 330}>
+          <Box
+            background={`url(/projects/wechelin/slogan.jpg)`}
+            backgroundSize="cover"
+            backgroundRepeat="no-repeat"
+            backgroundPosition="center"
           />
-        }
-      />
-      <NewExplainSection
-        backgroundColor="white"
-        color="black"
-        title={t('wechelin:title')}
-        descriptions={t('wechelin:summary.descriptions', { returnObjects: true })}
-        scopes={['Brand Design', 'Design Guidelines', 'UXUI', 'Mobile App']}
-        metaItems={
-          <>
-            <Box>
-              <Heading as="h3">
-                <CategoryTitle>{t('wechelin:summary.work.type')}</CategoryTitle>
-              </Heading>
-              {t('wechelin:summary.work.value')}
-            </Box>
-            <Box>
-              <Heading as="h3">
-                <CategoryTitle>PERIOD</CategoryTitle>
-              </Heading>
-              {t('wechelin:summary.period')}
-            </Box>
-          </>
-        }
-      />
-      <AspectRatio ratio={768 / 330}>
-        <Box
-          background={`url(/projects/wechelin/slogan.jpg)`}
-          backgroundSize="cover"
-          backgroundRepeat="no-repeat"
-          backgroundPosition="center"
+        </AspectRatio>
+        <BrandIdentitySection
+          title={t('wechelin:biSection.title')}
+          descriptions={t('wechelin:biSection.descriptions', { returnObjects: true })}
         />
-      </AspectRatio>
-      <BrandIdentitySection
-        title={t('wechelin:biSection.title')}
-        descriptions={t('wechelin:biSection.descriptions', { returnObjects: true })}
-      />
-      <BackgroundAndIssueSection
-        title={t('wechelin:backgroundSection.title')}
-        description={t('wechelin:backgroundSection.description')}
-      />
-      <UserResearchSection
-        title={t('wechelin:userResearchSection.title')}
-        description={t('wechelin:userResearchSection.description')}
-        questions={t('wechelin:userResearchSection.questions', { returnObjects: true })}
-        t={t}
-      />
-      <MissionSection
-        title={t('wechelin:missionSection.title')}
-        description={t('wechelin:missionSection.description')}
-      />
-      <ProblemAndSolutionSection
-        title={t('wechelin:problemAndSolutionSection.title')}
-        description={t('wechelin:problemAndSolutionSection.description')}
-        t={t}
-      />
-      <ProjectGoalSection
-        title={t('wechelin:projectGoalSection.title')}
-        description={t('wechelin:projectGoalSection.description')}
-        t={t}
-      />
-      <AspectRatio ratio={1920 / 336}>
-        <Box
-          background={`url(/projects/wechelin/banner-${locale}.jpg) no-repeat center`}
-          backgroundSize="contain"
-          aria-hidden
+        <BackgroundAndIssueSection
+          title={t('wechelin:backgroundSection.title')}
+          description={t('wechelin:backgroundSection.description')}
         />
-      </AspectRatio>
-      <FlowChartSection
-        title={t('wechelin:flowchartSection.title')}
-        description={t('wechelin:flowchartSection.description')}
-      />
-      <SplashSection
-        title={t('wechelin:flowchartSection.title')}
-        description={t('wechelin:flowchartSection.description')}
-      />
-      <OnBoardingSection
-        title={t('wechelin:onboardingSection.title')}
-        description={t('wechelin:onboardingSection.description')}
-      />
-      <FindingSection
-        title={t('wechelin:findingSection.title')}
-        description={t('wechelin:findingSection.description')}
-      />
-      <MyChelinSection
-        title={t('wechelin:myMichelinSection.title')}
-        description={t('wechelin:myMichelinSection.description')}
-      />
-      <YourMichelinSection
-        title={t('wechelin:yourMichelinSection.title')}
-        description={t('wechelin:yourMichelinSection.description')}
-      />
-      <MyPageSection title={t('wechelin:mySection.title')} description={t('wechelin:mySection.description')} />
-      <FeedSection title={t('wechelin:feedSection.title')} description={t('wechelin:feedSection.description')} />
-      <DesignElementSection
-        title={t('wechelin:designElementSection.title')}
-        description={t('wechelin:designElementSection.description')}
-      />
-      <Flex justifyContent="space-between" backgroundColor="black" color="white">
-        <StyledBox width="50%" />
-        <VerticalLine />
-        <StyledBox width="50%" flex="1" justifyContent="flex-end">
-          <NextBox textAlign="right">
-            <Label>NEXT PROJECT</Label>
-            <Value>National Patriots And Veterans</Value>
-          </NextBox>
-          <Link href={`/project/national-patriots-and-veterans`} passHref prefetch={false}>
-            <a aria-label={`national patriots and veterans 페이지로 이동`} rel="next">
-              <StyledIcRightArrow alt="이동" />
-            </a>
-          </Link>
-        </StyledBox>
-      </Flex>
-      <Footer />
-    </Box>
+        <UserResearchSection
+          title={t('wechelin:userResearchSection.title')}
+          description={t('wechelin:userResearchSection.description')}
+          questions={t('wechelin:userResearchSection.questions', { returnObjects: true })}
+          t={t}
+        />
+        <MissionSection
+          title={t('wechelin:missionSection.title')}
+          description={t('wechelin:missionSection.description')}
+        />
+        <ProblemAndSolutionSection
+          title={t('wechelin:problemAndSolutionSection.title')}
+          description={t('wechelin:problemAndSolutionSection.description')}
+          t={t}
+        />
+        <ProjectGoalSection
+          title={t('wechelin:projectGoalSection.title')}
+          description={t('wechelin:projectGoalSection.description')}
+          t={t}
+        />
+        <AspectRatio ratio={1920 / 336}>
+          <Box
+            background={`url(/projects/wechelin/banner-${locale}.jpg) no-repeat center`}
+            backgroundSize="contain"
+            aria-hidden
+          />
+        </AspectRatio>
+        <FlowChartSection
+          title={t('wechelin:flowchartSection.title')}
+          description={t('wechelin:flowchartSection.description')}
+        />
+        <SplashSection
+          title={t('wechelin:flowchartSection.title')}
+          description={t('wechelin:flowchartSection.description')}
+        />
+        <OnBoardingSection
+          title={t('wechelin:onboardingSection.title')}
+          description={t('wechelin:onboardingSection.description')}
+        />
+        <FindingSection
+          title={t('wechelin:findingSection.title')}
+          description={t('wechelin:findingSection.description')}
+        />
+        <MyChelinSection
+          title={t('wechelin:myMichelinSection.title')}
+          description={t('wechelin:myMichelinSection.description')}
+        />
+        <YourMichelinSection
+          title={t('wechelin:yourMichelinSection.title')}
+          description={t('wechelin:yourMichelinSection.description')}
+        />
+        <MyPageSection title={t('wechelin:mySection.title')} description={t('wechelin:mySection.description')} />
+        <FeedSection title={t('wechelin:feedSection.title')} description={t('wechelin:feedSection.description')} />
+        <DesignElementSection
+          title={t('wechelin:designElementSection.title')}
+          description={t('wechelin:designElementSection.description')}
+        />
+        <Flex justifyContent="space-between" backgroundColor="black" color="white">
+          <StyledBox width="50%" />
+          <VerticalLine />
+          <StyledBox width="50%" flex="1" justifyContent="flex-end">
+            <NextBox textAlign="right">
+              <Label>NEXT PROJECT</Label>
+              <Value>National Patriots And Veterans</Value>
+            </NextBox>
+            <Link href={`/project/national-patriots-and-veterans`} passHref prefetch={false}>
+              <a aria-label={`national patriots and veterans 페이지로 이동`} rel="next">
+                <StyledIcRightArrow alt="이동" />
+              </a>
+            </Link>
+          </StyledBox>
+        </Flex>
+        <Footer />
+      </Box>
+    </>
   )
 }
 
