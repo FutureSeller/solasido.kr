a696e6eb3e6ede1053ad05f9f621e40831608ac8
FutureSeller <f.s3ll3r@gmail.com>
2022-07-04 17:54:15 +0900

    fix: [WIP] add slugs

--- pages/sitemap/project.xml/index.tsx
+++ pages/sitemap/project.xml/index.tsx
@@ -1,33 +1,41 @@
 import { getServerSideSitemap } from 'next-sitemap'
-import fs from 'fs'
 
 import { generateSitemap } from '../../../utils/sitemapBuilder'
 
 import type { GetServerSideProps } from 'next'
 
-const SLUG_REGEX = /^\d{0,3}_(\S+)\.(jpg|png)$/
+const SLUGS = [
+  'korean-folk-village',
+  'ed',
+  'korean-traditional-pattern',
+  'welivearth',
+  'ovis',
+  'chemstree',
+  'citizen-politics-society',
+  'young-doctors-forum',
+  'ku-farm',
+  'ku-innovation',
+  'ku-40th-sejong-campus',
+  'ku-115th',
+  'hwayeon',
+  'lavida',
+  'global-order',
+  'hind',
+  'there',
+  'christmas-2021',
+  'national-patriots-and-veterans',
+  'wechelin',
+]
 
 export const getServerSideProps: GetServerSideProps = async ctx => {
   console.log('[Sitemap] generating start!')
-
-  const mainImageUrls = fs.readdirSync('./public/images/main')
-  const pathanames = mainImageUrls
-    .map(url => {
-      const matched = url.match(SLUG_REGEX)
-      if (!matched || matched.length < 2) {
-        return undefined
-      }
-
-      return `/${matched[1]}`
-    })
-    .filter(Boolean)
 
   ctx.res.setHeader('Cache-Control', 'public, maxage=60, stale-while-revalidate=600')
 
   return {
     ...getServerSideSitemap(
       ctx,
-      pathanames.map(pathname => generateSitemap(pathname!))
+      SLUGS.map(slug => generateSitemap(`/project/${slug}`))
     ),
   }
 }
