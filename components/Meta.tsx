5b280c86e1eb902edf2fe1d7ce3f456cb1305e3d
FutureSeller <f.s3ll3r@gmail.com>
2022-07-04 18:40:05 +0900

    feat: add canonical url

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -1,4 +1,13 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
+
+const DOAAIN = 'https://solasido.design'
+
+const getCanonicalUrl = (router: ReturnType<typeof useRouter>) => {
+  const locale = router.locale === 'ko' ? '' : `/${router.locale}`
+
+  return `${DOAAIN}${locale}${router.pathname}`.replace(/\/+$/, '')
+}
 
 interface Props {
   title: string
@@ -7,8 +16,12 @@ interface Props {
 }
 
 export default function Meta({ title, description, imageUrl }: Props) {
+  const router = useRouter()
+
   return (
     <Head>
+      <title>{title}</title>
+      <link rel="canonical" href={getCanonicalUrl(router)} />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="description" content={description} />
@@ -20,7 +33,6 @@ export default function Meta({ title, description, imageUrl }: Props) {
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       {imageUrl && <meta name="twitter:image" content={imageUrl} />}
-      <title>{title}</title>
     </Head>
   )
 }
